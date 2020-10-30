8998dd71a098e8fb876fcbdea6ba38d1e55e179d
mike <mikesapienzaaudio@gmail.com>
2020-10-30 13:24:49 -0500

    cards built no buttons workin

--- pokemon-teams-backend/app/models/pokemon.rb
+++ pokemon-teams-backend/app/models/pokemon.rb
@@ -1,3 +1,10 @@
 class Pokemon < ApplicationRecord
   belongs_to :trainer
+
+  # validate: :too_many_pokes
+
+  # def too_many_pokes
+
+  # end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -7,8 +7,30 @@ function main(){
     renderTrainers()
 }
 
+function renderPokemon(pokemons, pokeList){    
+    pokemons.forEach(poke => {
+    pokeList.innerHTML += `<li>${poke.nickname} (${poke.species})<button class="release" data-pokemon-id=${poke.id}>Release</button></li>`
+
+    })
+}
+
 function renderTrainers (){
 
+    let trainerList = document.querySelector('main') 
     
-
+    fetch('http://localhost:3000/trainers')
+    .then(resp => resp.json())
+    .then(trainers => {        
+        Object.values(trainers).forEach(trainer => {
+            trainerList.innerHTML += `<div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
+            <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+            <ul id=${trainer.id}> 
+            </ul>
+          </div>`
+          let pokeList = document.getElementById(trainer.id)
+          renderPokemon(trainer.pokemons, pokeList)
+        })
+    }) 
 }
+
+main()
