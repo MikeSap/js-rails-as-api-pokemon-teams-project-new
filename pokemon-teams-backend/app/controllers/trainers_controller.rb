af78aade3d2dd6f63a3f6a09cf2aa84c3d5bddba
mike <mikesapienzaaudio@gmail.com>
2020-10-30 11:58:27 -0500

    addindex

--- pokemon-teams-backend/app/controllers/trainers_controller.rb
+++ pokemon-teams-backend/app/controllers/trainers_controller.rb
@@ -1,2 +1,17 @@
 class TrainersController < ApplicationController
+
+    def index
+        trainers = Trainer.all
+        render json: trainers
+    end
+
+    def show
+        trainer = Trainer.find_by(id: params[:id])
+        if trainer
+            render json: trainer, include: [id: trainer.id, name: trainer.name, pokemon: trainer.pokemons ]
+        else
+            render json: { message: 'Trainer not found' }
+        end
+    end
+
 end
