7d909fc318f1f2b66fabe6aaa429800d050e2c00
mike <mikesapienzaaudio@gmail.com>
2020-10-30 14:02:37 -0500

    in progress poke create

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -1,2 +1,8 @@
 class PokemonsController < ApplicationController
+    
+    def create
+        
+        Pokemon.create(species: "#{Faker::Games::Pokemon.name}", nickname: "#{Faker::Name.first_name}", trainer_id:  )
+    end
+
 end
--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -2,17 +2,16 @@ const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
 
-
 function main(){
     renderTrainers()
+    addClickListeners()
 }
 
 function renderPokemon(pokemons, pokeList){    
     pokemons.forEach(poke => {
     pokeList.innerHTML += `<li>${poke.nickname} (${poke.species})<button class="release" data-pokemon-id=${poke.id}>Release</button></li>`
-
     })
-}
+}   
 
 function renderTrainers (){
 
@@ -23,14 +22,37 @@ function renderTrainers (){
     .then(trainers => {        
         Object.values(trainers).forEach(trainer => {
             trainerList.innerHTML += `<div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
-            <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+            <button class = 'add-btn' data-trainer-id="${trainer.id}">Add Pokemon</button>
             <ul id=${trainer.id}> 
             </ul>
-          </div>`
+            </div>`
           let pokeList = document.getElementById(trainer.id)
           renderPokemon(trainer.pokemons, pokeList)
         })
     }) 
 }
+
+
+function addClickListeners(){
+    document.addEventListener('click', function(e){        
+        if (e.target.className === 'add-btn'){
+            fetch('http://localhost:3000/pokemons', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                   trainer_id:  e.target.dataset.id
+                })   
+            })
+            .then(resp => resp.json())
+            .then(newPoke => {
+                debugger
+            })
+        }
+    })
+}
+
+
 
 main()
